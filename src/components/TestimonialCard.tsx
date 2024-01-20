8b81a054b84b6fbaa9ed275a0787304e82ad5bc9
have01 <have010101@gmail.com>
2024-01-20 11:35:30 +0530

    card responsive

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -11,14 +11,14 @@ const TestimonialCard: React.FC<{
 }> = ({ item, index }) => {
   return (
     <>
-      <div className="flex flex-col max-w-sm mx-2 my-6 h-[280px] bg-white shadow-lg justify-between">
-        <div className="px-2 py-4 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
-          <p className="relative  py-1 text-md text-center  dark:text-gray-100">
+      <div className="flex flex-col max-w-[300px] sm:max-w-sm mx-2 my-6 h-[280px] bg-white shadow-lg justify-between">
+        <div className="px-2 py-4 rounded-t-lg sm:px-8 md:px-12">
+          <p className="relative  py-1 text-md text-center ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
               fill="currentColor"
-              className="w-4 h-4 text-violet-400  dark:text-violet-400"
+              className="w-4 h-4 text-violet-400  "
             >
               <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
               <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
@@ -28,14 +28,14 @@ const TestimonialCard: React.FC<{
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
               fill="currentColor"
-              className="absolute right-0 w-4 h-4 text-violet-400  dark:text-violet-400"
+              className="absolute right-0 w-4 h-4 text-violet-400"
             >
               <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
               <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
             </svg>
           </p>
         </div>
-        <div className="flex flex-col items-center justify-center text-white p-4 m bg-[#5a51df] dark:bg-violet-400 dark:text-gray-900">
+        <div className="flex flex-col items-center justify-center text-white p-4 m bg-[#5a51df] ">
           <img
             src={`https://source.unsplash.com/50x50/?portrait?${index}`}
             alt=""
