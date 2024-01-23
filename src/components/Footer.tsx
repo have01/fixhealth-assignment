69bd456a4b339a15172e867cacfe9bd1bd1eadbf
have01 <have010101@gmail.com>
2024-01-23 12:38:27 +0530

    2024

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -70,7 +70,7 @@ const Footer = () => {
                         </a>
                     </div>
                     <p className="mt-8 text-base leading-6 text-center text-gray-400">
-                        © 2021 SomeCompany, Inc. All rights reserved.
+                        © 2024 Fixhealth, Inc. All rights reserved.
                     </p>
                 </div>
             </section></>
